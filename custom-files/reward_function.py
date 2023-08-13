bf3f3ee9d5c49a4d8d496378d8c2be5a3c6fca33
amithsbhat <64294728+amithsbhat@users.noreply.github.com>
2023-08-13 09:09:17 +0530

    Update reward_function.py

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,33 +1,136 @@
-def reward_function(params):
-    '''
-    Example of penalize steering, which helps mitigate zig-zag behaviors
-    '''
-    
-    # Read input parameters
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+import math
 
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
 
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
+def dist(point1, point2):
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+
+def rect(r, theta):
+    """
+    MUDR23-162
+    theta in degrees
+
+    returns tuple; (float, float); (x,y)
+    """
+
+    x = r * math.cos(math.radians(theta))
+    y = r * math.sin(math.radians(theta))
+    return x, y
+
+
+def polar(x, y):
+    """
+    MUDR23-162
+    returns r, theta(degrees)
+    """
+
+    r = (x ** 2 + y ** 2) ** .5
+    theta = math.degrees(math.atan2(y,x))
+    return r, theta
+
+
+def angle_mod_360(angle):
+    """
+    MUDR23-162
+    Maps an angle to the interval -180, +180.
+
+    Examples:
+    angle_mod_360(362) == 2
+    angle_mod_360(270) == -90
+
+    :param angle: angle in degree
+    :return: angle in degree. Between -180 and +180
+    """
+
+    n = math.floor(angle/360.0)
+
+    angle_between_0_and_360 = angle - n*360.0
+
+    if angle_between_0_and_360 <= 180.0:
+        return angle_between_0_and_360
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        return angle_between_0_and_360 - 360
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
 
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+def get_waypoints_ordered_in_driving_direction(params):
+    # waypoints are always provided in counter clock wise order
+    if params['is_reversed']: # driving clock wise.
+        return list(reversed(params['waypoints']))
+    else: # driving counter clock wise.
+        return params['waypoints']
 
-    return float(reward)
+
+def up_sample(waypoints, factor):
+    """
+    MUDR23-162
+    Adds extra waypoints in between provided waypoints
+
+    :param waypoints:
+    :param factor: integer. E.g. 3 means that the resulting list has 3 times as many points.
+    :return:
+    """
+    p = waypoints
+    n = len(p)
+
+    return [[i / factor * p[(j+1) % n][0] + (1 - i / factor) * p[j][0],
+             i / factor * p[(j+1) % n][1] + (1 - i / factor) * p[j][1]] for j in range(n) for i in range(factor)]
+
+
+def get_target_point(params):
+    waypoints = up_sample(get_waypoints_ordered_in_driving_direction(params), 20)
+
+    car = [params['x'], params['y']]
+
+    distances = [dist(p, car) for p in waypoints]
+    min_dist = min(distances)
+    i_closest = distances.index(min_dist)
+
+    n = len(waypoints)
+
+    waypoints_starting_with_closest = [waypoints[(i+i_closest) % n] for i in range(n)]
+
+    r = params['track_width'] * 0.9
+
+    is_inside = [dist(p, car) < r for p in waypoints_starting_with_closest]
+    i_first_outside = is_inside.index(False)
+
+    if i_first_outside < 0:  # this can only happen if we choose r as big as the entire track
+        return waypoints[i_closest]
+
+    return waypoints_starting_with_closest[i_first_outside]
+
+
+def get_target_steering_degree(params):
+    tx, ty = get_target_point(params)
+    car_x = params['x']
+    car_y = params['y']
+    dx = tx-car_x
+    dy = ty-car_y
+    heading = params['heading']
+
+    _, target_angle = polar(dx, dy)
+
+    steering_angle = target_angle - heading
+
+    return angle_mod_360(steering_angle)
+
+
+def score_steer_to_point_ahead(params):
+    best_stearing_angle = get_target_steering_degree(params)
+    steering_angle = params['steering_angle']
+
+    error = (steering_angle - best_stearing_angle) / 60.0  
+
+    score = 2*((1.0 - abs(error))**2)
+    
+    score += 2.0 *params["speed"]
+    
+    score += 10.0 * params['progress']
+    
+    if params['progress'] >= 100.0:
+        score += 100
+    return max(score, 0.01) 
+
+
+def reward_function(params):
+    return float(score_steer_to_point_ahead(params))
+
